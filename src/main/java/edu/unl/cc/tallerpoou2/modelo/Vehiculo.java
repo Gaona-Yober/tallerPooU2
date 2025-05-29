7d47d0f5e1746bf4fc385088ddca729c46a257c5
wilsonpalma <wilsonpalmasamaniego@gmail.com>
2025-05-29 15:43:27 -0500

    agregando los paquetes

---
+++ src/main/java/edu/unl/cc/tallerpoou2/modelo/Vehiculo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.unl.cc.tallerpoou2.modelo;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Wilson Palma
+ */
+public class Vehiculo {
+    private String placa;
+    private float capacidadCarga;
+    private float consumoCombustible;
+    private LocalDate fechaMantenimiento;    
+}
