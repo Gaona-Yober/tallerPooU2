88213764b7897052bb92f8f372d3b421ce2dd941
wilsonpalma <wilsonpalmasamaniego@gmail.com>
2025-05-29 20:21:31 -0500

    Se agrego el contenido de la clase VehiculoFacade y se creo la clase Viaje

--- src/main/java/edu/unl/cc/tallerpoou2/business/VehiculoFacade.java
+++ src/main/java/edu/unl/cc/tallerpoou2/business/VehiculoFacade.java
@@ -4,10 +4,33 @@
  */
 package edu.unl.cc.tallerpoou2.business;
 
+import edu.unl.cc.tallerpoou2.data.VehiculoRepository;
+import edu.unl.cc.tallerpoou2.modelo.Vehiculo;
+import edu.unl.cc.tallerpoou2.modelo.Viaje;
+import java.time.LocalDate;
+
 /**
  *
  * @author Gaona Yober, Palma Wilson, Ortega Fernando, Chimbo Camila
  */
 public class VehiculoFacade {
+    public static float precioCombustible;
+    private final VehiculoRepository vehiculoRepository = new VehiculoRepository();
     
+    public Vehiculo registrarVehiculo(Vehiculo vehiculo){
+            Vehiculo vehiculoPersisted = vehiculoRepository.save(vehiculo);
+            return vehiculoPersisted;
+    }
+    
+    public float calcularCostoViaje(Vehiculo vehiculo, Viaje viaje){
+        float Costo = viaje.getDistancia() * 
+                vehiculo.getConsumoCombustible() * 
+                VehiculoFacade.precioCombustible + 
+                vehiculoRepository.findCostoMantenimiento(vehiculo.getClass().getName());
+        return Costo;
+    }
+
+    public void actualizarMantenimiento(LocalDate fecha, Vehiculo vehiculo){
+        vehiculo.setFechaMantenimiento(fecha);
+    }
 }
